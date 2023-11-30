b65ea25bfb954690f7d827c8bfbb83a555da4b15
Elective <137693283+Elective-0@users.noreply.github.com>
2023-11-30 22:10:11 +0800

    Update main.ts

--- Ax/main.ts
+++ Ax/main.ts
@@ -4,23 +4,29 @@ let x = 0
 
 input.onButtonPressed(Button.A, function () {
     x += 1
+    if (x == 0) {
+        basic.showString("T")
+    } else if (x == 1) {
+        basic.showString("H")
+    } else if (x == 2) {
+        basic.showString("K")
+    } else if (x == 3) {
+        basic.showString("X")
+    }
+    basic.clearScreen()
 })
 
 basic.forever(function () {
     if (x == 0) {
-        basic.showString("T:")
         basic.showNumber(Math.floor(weatherbit.temperature() / 100))
         basic.clearScreen()
     } else if (x == 1) {
-        basic.showString("H:")
         basic.showNumber(Math.floor(weatherbit.humidity() / 1024))
         basic.clearScreen()
     } else if (x == 2) {
-        basic.showString("D:")
         basic.showString(weatherbit.windDirection())
         basic.clearScreen()
     } else if (x == 3) {
-        basic.showString("S:")
         basic.showNumber(Math.trunc(weatherbit.windSpeed()))
         basic.clearScreen()
     } else {
